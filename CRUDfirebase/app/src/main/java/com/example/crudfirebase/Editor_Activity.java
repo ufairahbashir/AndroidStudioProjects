ba161738ee5896d930d922c54f8b455aeefdcd22
ufairahbashir <ufairahbashir@gmail.com>
2023-11-10 15:57:30 +0700

    crud firebase folder upload

---
+++ CRUDfirebase/app/src/main/java/com/example/crudfirebase/Editor_Activity.java
@@ -0,0 +1,14 @@
+package com.example.crudfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Editor_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+}
---
+++ CRUDfirebase/app/src/main/java/com/example/crudfirebase/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.crudfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private FloatingActionButton floatingActionButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        floatingActionButton = findViewById(R.id.floatingButton);
+    }
+}
